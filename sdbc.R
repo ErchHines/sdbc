b4acc29701b86994f0daf0ef0adfaba6aff02784
Erich Hines <erich.hines@gmail.com>
2018-10-06 15:37:35 -0400

    Only the necessary files

--- sdbc.R
+++ sdbc.R
@@ -1,8 +1,11 @@
 library(xlsx)
 library(lubridate)
 library(dplyr)
+library(data.table)
 
-clients <- read.xlsx("E://Loki Project//All SBDC clients 2018.xls", sheetName = "Report")
+clients <- read.xlsx("~//All SBDC clients 2018.xls", sheetName = "Report")
+jobs <- read.xlsx("~//Jobs.xls", sheetName = "Report")
+investment <- read.xlsx("~//Capital Investment.xls", sheetName = "Report")
 
 #Get today's date and time so we can determine how old a firm is
 today <- as.Date(Sys.Date(), format = 'POSIXct')
@@ -101,4 +104,81 @@ clients$Hours <- with(clients,
 #number of the factors, in this case simple math will fix it though
 clients$Hours <- (clients$Hours * 5) - 5
 
+#Create a variable that gives how long the firm has been a client
+clients$Client.Since <- 
+  as.numeric(
+    difftime(today, clients$Client.Signature.Start.Date, units = "weeks")
+  )/52.25
+
+#Assuming a new clients is a client that has been signed a year or less
+clients$New.Client.Hours <- ifelse(clients$Client.Since <= 1, clients$Hours, 0)
+clients$Existing.Hours <- ifelse(clients$Client.Since > 1, clients$Hours, 0)
+
+#Change factirs to numbers
+jobs$Milestone.Amount <- as.numeric(as.character(jobs$Milestone.Amount))
+jobs$Change <- as.numeric(as.character(jobs$Change))
+
+jobs[jobs$Type %in% "NewBus",]$Change <- 1
+
+#Cast out miltestones and change numbers into seperate tables
+milestones <- dcast(jobs, Client.ID ~ Type, sum, value.var =  "Milestone.Amount")
+change <- dcast(jobs, Client.ID ~ Type, sum, value.var = "Change")
+
+#Get the number of reports by client
+reports <- as.data.frame(table(jobs$Client.ID))
+
+#Merge in the number of reports into the clients database
+clients <- merge(
+  x = clients, y = reports,
+  by.x = "Client.ID", by.y = "Var1", all.x = TRUE
+)
+
+#Change the name of the column to Reports and remove NAs
+names(clients)[names(clients) == "Freq"] <- "Reports"
+clients$Reports[is.na(clients$Reports)] <- 0
+
+#Merge in investment data for amount approved
+clients <- merge(
+  x = clients, y = investment[c("Client.ID","Amount.Approved")],
+  by = "Client.ID", all.x = TRUE
+)
+
+#If number of hours equals 0 report just the amount, look and see if this 
+#goes away once we get a better idea of how hours are calculated
+clients$Amount.Per.Hour <- ifelse(
+  clients$Hours == 0, clients$Amount.Approved, 
+  clients$Amount.Approved / clients$Hours
+)
+
+# Merge in the change table
+clients <- merge(
+  x = clients, y = change,
+  by = "Client.ID", all.x = TRUE
+)
+
+#Get the total number of jobs added
+clients$ChgStaff.All <- clients$`ChgStaff FT` + clients$`ChgStaff PT`
+
+#Again, check to see what to do when hours = 0
+clients$Sales.Per.Hour <- ifelse(
+  clients$Hours == 0, clients$ChgSales,
+  clients$ChgSales / clients$Hours
+)
+
+#Get business starts per hour
+clients$NewBus.Per.Hour <- ifelse(
+  clients$Hours == 0, clients$NewBus,
+  clients$NewBus / clients$Hours
+)
+
+#Milestones per hour
+clients$Milestones.Per.Hour <- ifelse(
+  clients$Hours == 0, clients$Reports,
+  clients$Reports / clients$Hours
+)
+
+#Get Business Start Contribution
+clients$Buesiness.Start.Contrib <- clients$NewBus / sum(clients$NewBus, na.rm = TRUE)
+
+clients$Buesiness.Start.Contrib <- clients$NewBus / sum(clients$NewBus, na.rm = TRUE)
 
